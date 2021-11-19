3f234c19e3bf3edfb5395de397dfb6ed6360d638
Aaditya Shete <aadityashete@gmail.com>
2021-11-19 17:11:04 +0530

    Added small desprition on images

--- src/Components/Work/WorkCard.tsx
+++ src/Components/Work/WorkCard.tsx
@@ -13,8 +13,12 @@ const WorkCard = (props:WorkCardProps) =>{
     return(
         <span className={`w-full  lg:w-workcard relative bg-purple-100 h-100 rounded-lg overflow-hidden ${props.class}`+ (props.loading? ' animate-pulse':'')} >
            {!props.loading && <img className="w-full  h-full object-cover" src={props.imgUrl} alt="test"/>}
-           <span className="absolute bottom-1/4 left-1/10 text-2xl text-white" >
+           <span className="absolute bottom-1/4 left-1/10 text-2xl text-white flex flex-col" >
                 {props.title}
+                <span>
+                    {props.service}
+                </span>
+
             </span> 
         </span> 
     );
